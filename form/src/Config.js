d9df5bf58fb1471e53e70ce5861419b0567271ef
UditPatel10 <udipatel10@gmail.com>
2022-07-15 18:04:37 +0530

    3rd commit

--- form/src/Config.js
+++ form/src/Config.js
@@ -1,6 +1,6 @@
 export const config= {
 
-    appId: 'ab3a92f2-c2aa-4b74-961d-36ec6904a6af',
+    appId: 'your azure app id',
     redirectUrl: 'http://localhost:3000/',
     scopes:[
         'user.read'
